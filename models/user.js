36f99f0eeab23ccb9bd8721a161dc882e0249ebe
Hesustus <gztale12@gmail.com>
2026-09-17 20:18:14 +0300

    fix lab3

--- models/user.js
+++ models/user.js
@@ -3,28 +3,47 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    static associate(models) {}
+    static associate(models) {
+      User.belongsToMany(models.Site, {
+        through: models.SiteUser,
+        foreignKey: 'userId',
+        otherKey: 'siteId',
+        as: 'sites'
+      });
+    }
   }
+
   User.init({
     email: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(255),
       unique: true,
       allowNull: false,
       validate: { isEmail: true }
     },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
     passwordHash: {
-      type: DataTypes.STRING,
-      allowNull: false
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      field: 'password_hash'
+    },
+    csrfToken: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      field: 'csrf_token'
     }
   }, {
     sequelize,
     modelName: 'User',
-    tableName: 'users',       
-    underscored: true,    
-    underscoredAll: true,  
+    tableName: 'users',
+    underscored: true,
+    underscoredAll: true,
     timestamps: true,
-    createdAt: 'created_at',   
-    updatedAt: false   
+    createdAt: 'created_at',
+    updatedAt: false
   });
+
   return User;
 };
